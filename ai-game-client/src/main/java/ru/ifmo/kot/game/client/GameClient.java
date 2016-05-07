a3545661b65cd35a98bf163e6f99836ec0bdab6d
Ilya Kisliakovsky <ilya.kisliakovsky@emc.com>
2016-05-07 17:32:51 +0300

    js  and java client connections

--- ai-game-client/src/main/java/ru/ifmo/kot/game/client/GameClient.java
+++ ai-game-client/src/main/java/ru/ifmo/kot/game/client/GameClient.java
@@ -3,14 +3,26 @@ package ru.ifmo.kot.game.client;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.websocket.*;
+import javax.websocket.ClientEndpoint;
+import javax.websocket.ContainerProvider;
+import javax.websocket.DeploymentException;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @ClientEndpoint
 public class GameClient {
 
+    private static List<GameClient> CLIENTS = new ArrayList<>();
     private static final Logger LOGGER = LogManager.getFormatterLogger(GameClient.class);
     private static final String GREETING = "The connection is open";
     private Session session;
@@ -49,12 +61,13 @@ public class GameClient {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         final WebSocketContainer webSocketContainer = ContainerProvider.getWebSocketContainer();
-        Session session = null;
+        webSocketContainer.setDefaultMaxSessionIdleTimeout(TimeUnit.SECONDS.toMillis(30));
         try {
-             session = webSocketContainer.connectToServer(GameClient.class,
+            webSocketContainer.connectToServer(GameClient.class,
                     URI.create(ClientConstants.SERVER_URL));
+            TimeUnit.SECONDS.sleep(30);
         } catch (DeploymentException | IOException e) {
             LOGGER.error("Failed to connect to the server");
         }
